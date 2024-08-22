2fcc979b3d06465328e0b1c3e737aab1f4c9175e
Tomas Baca <klaxalk@gmail.com>
2024-08-22 20:46:44 +0200

    wip

--- docker/custom_package/build_arm64.sh
+++ docker/custom_package/build_arm64.sh
@@ -6,4 +6,4 @@ REGISTRY=localhost:5000
 docker buildx create --name container --driver=docker-container
 docker buildx build . --file Dockerfile --tag "$REGISTRY/$LOCAL_TAG" --platform=linux/arm64
 
-docker push "$REGISTRY/$LOCAL_TAG"
+# docker push "$REGISTRY/$LOCAL_TAG"
