5c9dac24fb7689547e2900bda7b0a4bb36e779ee
Tomas Baca <klaxalk@gmail.com>
2024-10-01 15:29:04 +0200

    updated gnss shared data

--- docker/shared_data/gnss/build_arm64.sh
+++ docker/shared_data/gnss/build_arm64.sh
@@ -10,5 +10,3 @@ docker buildx create --name container --driver=docker-container
 docker buildx build . --file Dockerfile --tag "$REGISTRY/$LOCAL_TAG" --platform=linux/arm64
 
 docker push "$REGISTRY/$LOCAL_TAG"
-
-# docker save "$REGISTRY/$LOCAL_TAG" --output $LOCAL_TAG.tar
---
+++ docker/shared_data/gnss/export_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker save fly4future/robofly:shared_data_gnss --output ~/docker/shared_data_gnss.tar
