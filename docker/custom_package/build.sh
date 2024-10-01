25bb3c2ca3e48d7f815c43014ab309b319d187b5
Tomas Baca <klaxalk@gmail.com>
2024-10-01 15:51:39 +0200

    wip user session

--- docker/custom_package/build.sh
+++ docker/custom_package/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LOCAL_TAG=robofly:custom_package
-REGISTRY=localhost:5000
+REGISTRY=fly4future
 
 docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --no-cache --progress=plain
 
