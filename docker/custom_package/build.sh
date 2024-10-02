ea2e845a137b886b33e4427a240c8856b0a77622
Tomas Baca <klaxalk@gmail.com>
2024-10-02 08:32:07 +0200

    solved caching for building a custom package

--- docker/custom_package/build.sh
+++ docker/custom_package/build.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 LOCAL_TAG=robofly:custom_package
-REGISTRY=fly4future
 
-docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --no-cache --progress=plain
+ARCH=arm64
+# ARCH=amd64
 
-# docker push $REGISTRY/$LOCAL_TAG
+WORKSPACE_PATH=etc/catkin_workspace
+CACHE_PATH=cache
+
+# initialize the cache
+mkdir -p ./${CACHE_PATH}/${WORKSPACE_PATH}
+
+# this first build compiles the contents of "src" and storest the intermediate
+# catkin workspace in ./cache
+docker build . --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --file Dockerfile --platform=linux/arm64
+
+# this second build takes the resulting workspace and storest in in a final image
+# that can be deployed to a drone
+docker build . --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --tag $LOCAL_TAG --platform=linux/arm64
