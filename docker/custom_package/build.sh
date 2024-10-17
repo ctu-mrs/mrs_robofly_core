4e131ee5ecee0e8ee61a9482b5092050ef3abf10
Tomas Baca <klaxalk@gmail.com>
2024-10-17 17:39:23 +0200

    polishing

--- docker/custom_package/build.sh
+++ docker/custom_package/build.sh
@@ -11,9 +11,11 @@ CACHE_PATH=cache
 # initialize the cache
 mkdir -p ./${CACHE_PATH}/${WORKSPACE_PATH}
 
+docker buildx select default
+
 # this first build compiles the contents of "src" and storest the intermediate
-docker build . --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --file Dockerfile --platform=linux/arm64
+docker build . --target stage_cache_workspace --output ./${CACHE_PATH} --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --file Dockerfile --platform=linux/$ARCH
 
 # this second build takes the resulting workspace and storest in in a final image
 # that can be deployed to a drone
-docker build . --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --tag $LOCAL_TAG --platform=linux/arm64
+docker build . --target stage_finalization --file Dockerfile --build-arg WORKSPACE_PATH=${WORKSPACE_PATH} --tag $LOCAL_TAG --platform=linux/$ARCH
--- docker/main_image/build_arm64.sh
+++ docker/main_image/build_arm64.sh
@@ -3,4 +3,4 @@
 LOCAL_TAG=ctumrs/mrs_uav_system:robofly
 REGISTRY=localhost:5000
 
-docker buildx build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=linux/arm64 --push
+docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=linux/arm64 --push
