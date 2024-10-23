8177e84e19ae9a9f1fa765411983e66d4ad59cef
Tomas Baca <klaxalk@gmail.com>
2024-10-23 13:40:48 +0200

    updating sessions

--- setup/modifier_docker_image/build_image.sh
+++ setup/modifier_docker_image/build_image.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-LOCAL_TAG=ctumrs/mrs_uav_system:robofly
+LOCAL_TAG=mrs_uav_system:custom
 REGISTRY=localhost:5000
+
+docker buildx use default
 
 docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=linux/arm64 --push
--- setup/robofly_docker_image/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build . --file Dockerfile --tag ctumrs/mrs_uav_system:latest
