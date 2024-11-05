423fe2133df66ffecf68562cd0de1f9e5c774c6e
Tomas Baca <klaxalk@gmail.com>
2024-11-05 11:24:59 +0100

    removed docker image modifier

--- setup/modifier_docker_image/build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LOCAL_TAG=mrs_uav_system:1.5.0
-REGISTRY=klaxalk:5000
-
-docker buildx use default
-
-docker build . --file Dockerfile --tag $REGISTRY/$LOCAL_TAG --platform=linux/arm64 --push --no-cache --progress=plain
